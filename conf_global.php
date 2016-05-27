7582bbecb9232c72b764f3b9e50439d95698051e
Ragnarok World <noriel.calingasan@yahoo.com>
2016-05-27 20:14:01 +0800

    a
    a

---
+++ conf_global.php
@@ -0,0 +1,18 @@
+<?php
+
+$INFO = array (
+  'sql_host' => 'localhost',
+  'sql_database' => 'wedevgamesph_ragnaboards',
+  'sql_user' => 'wedevgamesph',
+  'sql_pass' => '7iN2Gvq57g',
+  'sql_port' => 3306,
+  'sql_socket' => '/var/run/mysqld/mysqld.sock',
+  'sql_tbl_prefix' => '',
+  'sql_utf8mb4' => true,
+  'board_start' => 1459351837,
+  'installed' => true,
+  'base_url' => 'http://ragnaboards.wedevgames.ph',
+  'guest_group' => 2,
+  'member_group' => 3,
+  'admin_group' => 4,
+);
